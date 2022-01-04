a37ba06ed53fbc6270f651e6f2ea5ac3ccb82dc4
WallH <wallhotelcom@gmail.com>
2022-01-03 22:14:17 -0300

    Sancion front

--- src/app/models/sancion.ts
+++ src/app/models/sancion.ts
@@ -3,12 +3,13 @@ import { Usuario } from "./usuario";
 
 export class Sancion
 {
-    constructor(public oficial?:Usuario, public supervisor?:Usuario, public grado?:GradoSancion, public observacion?:String, public fecha?:Date, public fechaFin?:Date, public degrado?:Boolean, public _id?:string)
+    constructor(public oficial?:Usuario, public supervisor?:Usuario, public grado?:GradoSancion, public observacion?:String, public rangodurante?:string, public fecha?:Date, public fechaFin?:Date, public degrado?:Boolean, public _id?:string)
     {
         this.oficial = oficial;
         this.supervisor = supervisor;
         this.grado = grado;
         this.observacion = observacion;
+        this.rangodurante = rangodurante;
         this.fecha = fecha;
         this.fechaFin = fechaFin;
         this.degrado = degrado;
